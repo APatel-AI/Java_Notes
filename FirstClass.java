9a882af6cb820a09cc73ae11c201bc7427eb90fd
APatel-AI <arpan.p.se@gmail.com>
2024-12-01 14:17:18 -0600

    added notes

--- FirstClass.java
+++ FirstClass.java
@@ -1,4 +1,12 @@
+// Notes
+
+// The word "public" is a Java keyword which is called an access modifier
+// What does an access modifier do: it allows us to define which parts of our code, or even someone else's code, can access a particular element
 public class FirstClass { 
+    // what is a method: a collection of statements, one or more, that perform an operation.
+    // Below we are using the "main" method which is an entry point for any java code which java looks for to start and run the program.
+    // static is java keyword and a part of the OOP concept which will be included in the later notes but it is essential in helping java find the method "main" or any other custom method
+    // void is a java keyword used to indicated that the method will not return any values after executing.
     public static void main(String[] args) {
         System.out.print("Hello World");
     }
