3fb3326586cd4135e109f0d685d4b04384cb179a
APatel-AI <arpan.p.se@gmail.com>
2024-12-04 15:10:55 -0600

    updated notes with expressions

---
+++ StatementsWhiteSpaceAndIndenting/StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int myVariable = 50; // myVariable = 50 is the expression and "int myVariable = 50;" is the statement
+        myVariable ++; //with the semicolon this is a complete statement and without the semicolon it is just an expression
+        myVariable --;
+
+        System.out.println("This is a test");
+
+        //you can continue to print statement on mutiple lines
+        System.out.println("This is" +
+                " another" +
+                " still more."); // it is completed with the closing parenthesis and the semicolon
+    }
+}
