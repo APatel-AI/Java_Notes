52bacd35f9ca627129e52bf33ac806ee7cba175f
APatel-AI <arpan.p.se@gmail.com>
2024-12-08 11:52:34 -0600

    refactored code

--- MethodsChallenge/MethodsChallenge/src/MethodChallenge.java
+++ MethodsChallenge/MethodsChallenge/src/MethodChallenge.java
@@ -22,9 +22,9 @@ public class MethodChallenge{
             return 2;
         }else if (playerScore >= 100){
             return 3;
-        }else{
-            return 4;
         }
+
+        return 4;
 
     }
    
