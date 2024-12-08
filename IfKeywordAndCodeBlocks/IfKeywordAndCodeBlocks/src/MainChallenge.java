cae133f08b5b2ed7d175a8d137a33cbe7ed36545
APatel-AI <arpan.p.se@gmail.com>
2024-12-08 10:43:39 -0600

    updated java notes

--- IfKeywordAndCodeBlocks/IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,25 +1,39 @@
 public class MainChallenge {
     public static void main(String[] args){
-        boolean gameOver = true; 
-        int score = 800; 
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        int finalScore = score;
+        // The variables below are no longer required because of the calculateScore method
 
-        calculateScore(true, 800, 5, 100);
+        // boolean gameOver = true; 
+        // int score = 800; 
+        // int levelCompleted = 5;
+        // int bonus = 100;
+        // int finalScore = score;
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+    
+        // Call to method 1 
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("The highScore is " + highScore);
 
-        if (gameOver){ // same thing as gameOver == true
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+
+        // Call to method 2
+        System.out.println("The nex highScore is " +  
+        calculateScore(true, 10000, 8, 200));
+
+
+        // The actions below can be completed with the method, hence no longer needed.
+        // score = 10000;
+        // levelCompleted = 8;
+        // bonus = 200;
+
+        // if (gameOver){ // same thing as gameOver == true
+        //     finalScore += (levelCompleted * bonus);
+        //     System.out.println("Your final score was " + finalScore);
+        // }
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus ){
+
+    //define calculateScore method
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus ){
 
         int finalScore = score;
 
@@ -28,6 +42,8 @@ public class MainChallenge {
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
+
+        return finalScore;
 
     }
 
