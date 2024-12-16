4619fa98458c79a0a51a623cf77c85befa652bef
APatel-AI <arpan.p.se@gmail.com>
2024-12-16 17:21:04 -0600

    added enhanced version of switch

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,23 +1,39 @@
+
+
 public class Main{
     public static void main(String args[]){
 
         int switchValue = 3; 
 
-        switch (switchValue){
-            case 1:
-                System.out.println("Value was 1");
-                break;
+        //traditional switch statement
+        // switch (switchValue){
+        //     case 1:
+        //         System.out.println("Value was 1");
+        //         break;
 
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3:
-                System.out.println("Value was a 3, a 4, or a 5");
+        //     case 2:
+        //         System.out.println("Value was 2");
+        //         break;
+        //     case 3:
+        //         System.out.println("Value was a 3, a 4, or a 5");
+        //         System.out.println("Actually it was a " + switchValue);
+        //         break;
+        //     default:
+        //         System.out.println("Was not 1, 2, 3, 4, or 5");
+        //         break;
+        // }
+        
+        // enhanced switch statement
+        switch (switchValue){
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4, or 5");
-                break;
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+
         }
+
     }
 }
