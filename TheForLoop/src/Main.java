0cf564e81ff0a92ac7810e5c5c1908cf4765e306
APatel-AI <arpan.p.se@gmail.com>
2024-12-18 15:30:14 -0600

    added notes on java for loop

---
+++ TheForLoop/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        // prints 1-6
+        for(int counter = 1; counter <= 6; counter ++){
+            System.out.println(counter);
+        }
+    }
+}
